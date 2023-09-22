447a541ae3246fa1ffb008724475d57bd6478c0d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:35:01 -0300

    Tipos de dados

---
+++ tipos-dados.js
@@ -0,0 +1,14 @@
+//  Numeros inteiros 
+let idadeAuno = 18;
+
+// Números frutuantes
+let valorProduto = 5.35;
+
+//  String 
+let nomeAluno = "Pedro Henrique";
+
+//  Boolean
+let casado = true;
+
+// console.log(typeof casado);
+console.log(typeof idadeAuno)
