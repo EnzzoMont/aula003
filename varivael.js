d8bd0fb89a6da751603f229471457b82d364d91a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:11:26 -0300

    Adicionando variaveis

---
+++ varivael.js
@@ -0,0 +1,12 @@
+// Cria uma variável com valor indefinido
+let nome;
+
+// Cria uma variável vazia
+let disciplina = null;
+
+// Cria uma variável  com valor
+let texto = "Este é um exemplo";
+
+console.log(texto);
+
+
